1850a58b540dffdb92f9cfc6b7460dda085f90ff
StevePro7 <steveprozune@hotmail.com>
2018-12-20 18:06:45 +0000

    Add graphics

---
+++ dev/gfx.h
@@ -0,0 +1,54 @@
+extern const unsigned char	all_sprites_palette_bin[];
+#define				all_sprites_palette_bin_size 16
+
+extern const unsigned char	all_sprites_tiles_psgcompr[];
+#define				all_sprites_tiles_psgcompr_size 3700
+
+extern const unsigned char	background_tiles_palette_bin[];
+#define				background_tiles_palette_bin_size 13
+
+extern const unsigned char	background_tiles_psgcompr[];
+#define				background_tiles_psgcompr_size 490
+
+extern const unsigned char	black_castle_front_txt[];
+#define				black_castle_front_txt_size 216
+
+extern const unsigned char	black_castle_interior_txt[];
+#define				black_castle_interior_txt_size 216
+
+extern const unsigned char	font_fnt[];
+#define				font_fnt_size 768
+
+extern const unsigned char	garden_center_txt[];
+#define				garden_center_txt_size 218
+
+extern const unsigned char	garden_left_txt[];
+#define				garden_left_txt_size 218
+
+extern const unsigned char	garden_right_txt[];
+#define				garden_right_txt_size 218
+
+extern const unsigned char	green_dragon_lair_txt[];
+#define				green_dragon_lair_txt_size 216
+
+extern const unsigned char	labyrinth_bottom_txt[];
+#define				labyrinth_bottom_txt_size 218
+
+extern const unsigned char	labyrinth_entrance_txt[];
+#define				labyrinth_entrance_txt_size 218
+
+extern const unsigned char	labyrinth_left_txt[];
+#define				labyrinth_left_txt_size 218
+
+extern const unsigned char	labyrinth_middle_txt[];
+#define				labyrinth_middle_txt_size 218
+
+extern const unsigned char	labyrinth_top_txt[];
+#define				labyrinth_top_txt_size 218
+
+extern const unsigned char	yellow_castle_front_txt[];
+#define				yellow_castle_front_txt_size 216
+
+extern const unsigned char	yellow_castle_interior_txt[];
+#define				yellow_castle_interior_txt_size 216
+
